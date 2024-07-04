63440b1ed231d475fa606b375b095318d3491b82
samychowdhury01 <samychowdhury84@gmail.com>
2024-07-04 16:30:23 +0600

    úpdated the logic function

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -6,39 +6,31 @@ import { Order } from './order.model';
 // create a new product
 const createOrderIntoDB = async (order: TOrder) => {
   const product = await Product.findById(order.productId);
+
   if (!product) {
     throw new Error('Product not found');
-  } else if (
+  }
+
+  if (
     product?.inventory.quantity < order.quantity ||
     product?.inventory.quantity === 0
   ) {
     throw new Error('Insufficient quantity available in inventory');
-  } else {
-    const newQuantity = product.inventory.quantity - order.quantity;
-    const updatedProduct = await Product.findByIdAndUpdate(
-      { _id: order.productId },
-      {
-        $set: {
-          'inventory.quantity': newQuantity,
-        },
-      },
-      { new: true },
-    );
-    if (updatedProduct?.inventory.quantity === 0) {
-      await Product.findByIdAndUpdate(
-        { _id: order.productId },
-        {
-          $set: {
-            'inventory.inStock': false,
-          },
-        },
-        { new: true },
-      );
-    }
-
-    const newOrder = await Order.create(order);
-    return newOrder;
   }
+
+  const update = {
+    $inc: { 'inventory.quantity': -order.quantity },
+    $set: {
+      'inventory.inStock':
+      product.inventory.quantity - order.quantity !== 0
+
+    },
+  };
+
+  await Product.findByIdAndUpdate(order.productId, update, { new: true });
+
+  const newOrder = await Order.create(order);
+  return newOrder;
 };
 
 export const OrderServices = {
